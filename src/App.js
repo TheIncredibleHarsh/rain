7b131ef9a90f977f7f874f0540a9105f240551bb
TheIncredibleHarsh <harsh.singh343@gmail.com>
2021-05-29 18:08:48 +0530

    create project, add bg videos

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import BgRain from './bg-rain.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <BgRain />
+  );
+}
+
+export default App;
---
+++ src/bg-rain.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import "./App.css";
+import video1 from './assets/bg-video-1.mp4';
+import video2 from './assets/bg-video-2.mp4';
+import video3 from './assets/bg-video-3.mp4';
+import video4 from './assets/bg-video-4.mp4';
+import video5 from './assets/bg-video-5.mp4';
+
+const BgRain = () => {
+
+    // const [bgVideo, setBgVideo] = useState();
+
+    var bgVideos = [video1, video2, video3, video4, video5];
+
+    // setBgVideo(video1);
+
+    return <>
+        <div className="bg-rain-container">
+            <video id="bg-video" autoPlay loop muted>
+                <source src={bgVideos[Math.ceil(Math.random()*6)]} type="video/mp4" />
+            </video>
+        </div>
+        <div className="bg-overlay" />
+    </>
+}
+
+export default BgRain;
